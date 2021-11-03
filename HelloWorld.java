2de0a442298a60419daa27ac8e9c11b018c0c304
orkaLivet <93656671+orkaLivet@users.noreply.github.com>
2021-11-03 13:55:13 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	  System.out.println("Hello World!");
+      System.out.print("The answer to the ultimate question is ");
+      int x = 42; 
+      System.out.print(x);
+	}
+}
